c92a0813339af2ccd790c018b1fed63a69da78aa
Jack Xiao <xiao.jack@utexas.edu>
2025-09-16 21:39:36 +0800

    some changes

--- inst/julia/GIC_Model_Selection.jl
+++ inst/julia/GIC_Model_Selection.jl
@@ -105,7 +105,7 @@ function GIC_Variable_Selection(
 
             # GIC evaluation after addition
             #GIC_i = Calculate_GIC_short(Y, X_subsets, A_inv)
-            GIC_i, A_inv = Calculate_GIC(Y, X_subsets, Huber, P = n, gamma = gamma)
+            GIC_i, A_inv = Calculate_GIC(Y, X_subsets, n, gamma, Huber)
 
             if tr(GIC_c) < tr(GIC_i)  # Keep change if GIC improves
                 GIC_c = GIC_i
